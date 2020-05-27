2b60cd37ec3a22a0e04c088346b97336c5501515
davconde <davidcondemorales@gmail.com>
2020-05-28 01:08:06 +0200

    Fixed audio for Android

--- Pong/AudioManager.cs
+++ Pong/AudioManager.cs
@@ -6,34 +6,18 @@ using System.Text;
 
 namespace Pong {
     public static class AudioManager {
-#if WINDOWS
         public static AudioEngine AudioEngine;
         public static WaveBank WaveBank;
         public static SoundBank SoundBank;
-#elif ANDROID
-        public static Dictionary<string, SoundEffect> SoundEffects = new Dictionary<string, SoundEffect>();
-#endif
 
         public static void Initialize(ContentManager content) {
-#if WINDOWS
-            AudioEngine = new AudioEngine("Content/Sound/XACT/GameAudio.xgs");
-            WaveBank = new WaveBank(AudioEngine, "Content/Sound/XACT/Wave Bank.xwb");
-            SoundBank = new SoundBank(AudioEngine, "Content/Sound/XACT/Sound Bank.xsb");
-#elif ANDROID
-            SoundEffects.Add("Pad", content.Load<SoundEffect>("Sound/Pad"));
-            SoundEffects.Add("Score", content.Load<SoundEffect>("Sound/Score"));
-            SoundEffects.Add("Wall", content.Load<SoundEffect>("Sound/Wall"));
-#endif
+            AudioEngine = new AudioEngine("Content/Sound/GameAudio.xgs");
+            WaveBank = new WaveBank(AudioEngine, "Content/Sound/Wave Bank.xwb");
+            SoundBank = new SoundBank(AudioEngine, "Content/Sound/Sound Bank.xsb");
         }
 
         public static void PlayCue(string cueName) {
-#if WINDOWS
             SoundBank.PlayCue(cueName);
-#elif ANDROID
-            SoundEffect soundEffect;
-            SoundEffects.TryGetValue(cueName, out soundEffect);
-            soundEffect.Play();
-#endif
         }
     }
 }
